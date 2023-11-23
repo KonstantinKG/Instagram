7182cc68308e9f3f96a5158cdd9721f278de6511
konst <konstantingoryachkovskiy@gmail.com>
2023-11-23 10:04:12 +0600

    fix: updated docker-compose.yaml and entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
+
+dotnet-ef migrations remove -p ./Instagram.Infrastructure/ -s ./Instagram.WebApi/ -f
+
+dotnet-ef migrations add InitialCreate -p ./Instagram.Infrastructure/ -s ./Instagram.WebApi/
+
 # Apply database migrations
 dotnet-ef database update -p ./Instagram.Infrastructure/ -s ./Instagram.WebApi/
 
