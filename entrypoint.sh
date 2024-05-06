78f21f161c2444886f36c23ae3f91a669d9a12de
konst <konstantingoryachkovskiy@gmail.com>
2024-05-06 18:03:11 +0500

    Latest version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 MIGRATIONS_FOLDER="./Instagram.Infrastructure/Migrations/"
 file_count=$(find "$MIGRATIONS_FOLDER" -maxdepth 1 -type f | wc -l)
 
-dotnet-ef database update -p ./Instagram.Infrastructure/ -s ./Instagram.WebApi/
+if [ "$file_count" -eq "0" ]; then
+  echo "Creating new migration..."
+  dotnet-ef migrations add InitialCreate -p ./Instagram.Infrastructure/ -s ./Instagram.WebApi/
+  
+  echo "Updating database..."
+  dotnet-ef database update -p ./Instagram.Infrastructure/ -s ./Instagram.WebApi/
+fi
 
 exec dotnet run --project ./Instagram.WebApi
 
